79729d08374022694774b9fa7a3395beb867303d
Xubai Wang <18016038327@189.cn>
2025-12-09 19:04:38 +0800

    style: deny unwrap

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
+#![deny(clippy::unwrap_used)]
+
+#[doc(inline)]
+pub use crate::{browser::*, error::*, page::*, session::*};
+
 pub mod browser;
 pub mod error;
 pub mod page;
 pub mod session;
-
-#[doc(inline)]
-pub use crate::{browser::*, error::*, page::*, session::*};
